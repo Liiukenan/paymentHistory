e0c651d547bd90d1d1648f47f12c85173ae1c4b4
刘克难 <liukenan@liukenandeiMac.local>
2019-12-13 02:08:40 +0800

    生成vue默认文件

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+Vue.prototype.http = axios;
+Vue.config.productionTip = false;
+/*第一层if判断生产环境和开发环境*/
+if (process.env.NODE_ENV === 'production') {
+  /*第二层if，根据.env文件中的VUE_APP_FLAG判断是生产环境还是测试环境*/
+  if (process.env.VUE_APP_FLAG === 'pro') {
+    //production 生产环境
+    axios.defaults.baseURL = 'http://api.xinggeyun.com'; //路径
+  } else {
+    //test 测试环境
+    axios.defaults.baseURL = 'http://192.168.0.152:8102'; //路径
+  }
+} else { //dev 开发环境 axios.defaults.baseURL = 'http://192.168.0.152:8102';//路径
+  axios.defaults.baseURL = 'http://192.168.0.152:8102'
+}
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
